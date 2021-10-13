b7d1ac0eeefb57f83fc38b6543f3962fa51bf1c3
sangha <sangha0411@gmail.com>
2021-10-13 09:19:14 +0000

    Add preprocessor

---
+++ preprocessor.py
@@ -0,0 +1,13 @@
+import re
+
+class SenPreprocessor :
+    def __init__(self, tokenizer) :
+        assert hasattr(tokenizer, 'morphs')
+        self.tokenizer = tokenizer
+
+    def __call__(self, sen) :
+        assert isinstance(sen, str)
+        tok_list = self.tokenizer.morphs(sen)
+        sen = ' '.join(tok_list)
+        return sen
+ 
